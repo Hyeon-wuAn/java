07e6d55df8d3aac1246049c382a8179dbe160f36
jjang051globalit <zerox2519@gmail.com>
2023-05-01 17:49:45 +0900

    블라블라

--- day05/src/day05/Test.java
+++ day05/src/day05/Test.java
@@ -4,5 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("집에서 작업 했음");
 		System.out.println("학원에서 다시 작업 좀 헷갈림....");
+		System.out.println("test중");
 	}
 }
