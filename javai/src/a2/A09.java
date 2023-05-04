5d2c8bcf718437858bb9a7574de5684d710f7fbd
jjang051globalit <zerox2519@gmail.com>
2023-05-04 18:19:51 +0900

    simple

---
+++ javai/src/a2/A09.java
@@ -0,0 +1,15 @@
+package a2;
+
+public class A09 {
+	public static void main(String[] args) {
+		String number1 = new String("1234");
+		String number2 = new String("4321");
+		
+		System.out.println(number1 + number2);
+		
+		int num1 = Integer.parseInt(number1);
+		int num2 = Integer.parseInt(number2);
+		
+		System.out.println(num1+num2);
+	}
+}
